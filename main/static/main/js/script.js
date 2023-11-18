5aeb6d184fe64c529b99c6eefdc9d56aa0c1f448
OksanaBukartyk <oksana.petriv17@gmail.com>
2023-11-18 09:44:22 +0200

    add cards carusel

---
+++ main/static/main/js/script.js
@@ -0,0 +1,47 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+(function(){
+    // setup your carousels as you normally would using JS
+    // or via data attributes according to the documentation
+    // https://getbootstrap.com/javascript/#carousel
+    $('#carousel123').carousel({ interval: 2000 });
+    $('#carouselABC').carousel({ interval: 3600 });
+  }());
+  
+  (function(){
+    $('.carousel-showmanymoveone .item').each(function(){
+      var itemToClone = $(this);
+  
+      for (var i=1;i<4;i++) {
+        itemToClone = itemToClone.next();
+  
+        // wrap around if at end of item collection
+        if (!itemToClone.length) {
+          itemToClone = $(this).siblings(':first');
+        }
+  
+        // grab item, clone, add marker class, add to collection
+        itemToClone.children(':first-child').clone()
+          .addClass("cloneditem-"+(i))
+          .appendTo($(this));
+      }
+    });
+  }());
